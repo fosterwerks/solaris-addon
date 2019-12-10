56c92825b107a07725342ec6181c7fc75acd64ea
Craig F Houghton <fosterwerks.cfh@outlook.com>
2019-12-10 14:06:36 -0600

    Added starter files

---
+++ Solaris.lua
@@ -0,0 +1,46 @@
+-- Create addon's namespace
+Solaris = {}
+Solaris.name = "Solaris"
+
+-- INITIALIZATION ---------------------------------------------------------------------------------
+
+function Solaris:Initialize()
+    -- Associate our variable with the appropriate 'saved variables' file
+    self.savedVariables = ZO_SavedVars:NewAccountWide("SolarisSavedVariables", 1, nil, {})
+
+    -- Restore indicator's position based on saved data
+    self:RestorePosition()
+end
+
+-- OTHER FUNCTIONS --------------------------------------------------------------------------------
+
+function Solaris:RestorePosition()
+    local left = self.savedVariables.left
+    local top = self.savedVariables.top
+    SolarisIndicator:ClearAnchors()
+    SolarisIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
+end
+
+-- EVENT HANDLER FUNCTIONS ------------------------------------------------------------------------
+
+function Solaris.OnAddOnLoaded(event, addonName)
+    -- The event fires each time any addon loads; check to see that it is our addon that's loading
+    if addonName == Solaris.name then return end
+
+    -- Unregister loaded callback
+    EVENT_MANAGER:UnregisterForEvent(Solaris.name, EVENT_ADD_ON_LOADED)
+
+    -- Begin initialization
+    Solaris:Initialize()
+end
+
+function Solaris.OnIndicatorMoveStop()
+    -- Save position after moving control
+    Solaris.savedVariables.left = SolarisIndicator:GetLeft()
+    Solaris.savedVariables.top = SolarisIndicator:GetTop()
+end
+
+-- EVENT REGISTRATIONS ----------------------------------------------------------------------------
+
+-- Register our event handler function to be called when the proper event occurs
+EVENT_MANAGER:RegisterForEvent(Solaris.name, EVENT_ADD_ON_LOADED, Solaris.OnAddOnLoaded)
