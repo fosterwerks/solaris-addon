537a6fae9679ba6096607a96cce23fd0c0ee929e
Craig F Houghton <fosterwerks.cfh@outlook.com>
2020-01-01 12:56:19 -0600

    Added Time functions and RTI position at init

--- Solaris.lua
+++ Solaris.lua
@@ -1,4 +1,3 @@
--- Create addon's namespace
 Solaris = {}
 Solaris.name = "Solaris"
 Solaris.version = 1
@@ -6,6 +5,54 @@ Solaris.version = 1
 local defaults = {
     numberOfDays = 3
 }
+
+-- CONSTANTS --------------------------------------------------------------------------------------
+
+local PERCENT_NIGHT = 7200 / 20955
+local PERCENT_DAY = 1 - PERCENT_NIGHT
+local PERCENT_TT = 20955 / (24 * 60 * 60)
+
+local SECONDS_PER_DAY = 86400
+
+local WINDOW_LENGTH = 480
+
+-- TIME FUNCTIONS ---------------------------------------------------------------------------------
+
+function Solaris.GetSecondsRT()
+    local t = GetFormattedTime()
+    local s = t % 100           t = math.floor(t/100)
+    s = s + (t % 100) * 60      t = math.floor(t/100)
+    s = s + (t * 3600)
+    return s
+end
+
+function Solaris.GetPercentRT()
+    local t = GetFormattedTime()
+    local s = t % 100
+    t = (t - s) / 100
+    local m = t % 100
+    local h = (t - m) / 100
+    m = m + (s / 60)
+    h = h + (m / 60)
+    return h / 24
+end
+
+function Solaris.GetHMS(percentOfDay)
+    local t = percentOfDay * 24
+    local h = math.floor(t) t=(t-h)*60
+    local m = math.floor(t) t=(t-m)*60
+    local s = math.floor(t)
+    return h, m, s
+end
+
+function Solaris.GetPercentTT()
+	local day,night=20955,7200
+	local tSinceMidnight=GetTimeStamp() - 1398044126 + night/2 + (day-night)/2
+	local daysPast=day*math.floor(tSinceMidnight/day)
+	local s=tSinceMidnight-daysPast
+    local t=s/day
+    return t
+end
 
 -- INITIALIZATION ---------------------------------------------------------------------------------
 
@@ -15,7 +62,14 @@ function Solaris:Initialize()
 
     self:RestorePosition()
     self:BuildControls()
+    self:RegisterSlashCommands()
 end
+
+function Solaris:RegisterSlashCommands()
+
+end
+
+
 
 -- GUI FUNCTIONS ----------------------------------------------------------------------------------
 
@@ -27,8 +81,17 @@ function Solaris:RestorePosition()
 end
 
 function Solaris:BuildControls()
-    
+    local window = SolarisTimelineControl
+    local w, _ = window:GetDimensions()
+
+    local p = Solaris.GetPercentRT() * w / 2
+    local rti = window:GetNamedChild("RT_Indicator")
+    rti:ClearAnchors()
+    rti:SetAnchor(BOTTOM, window, TOPLEFT, p, 0)
+
+    local tDay_1 = CreateControlFromVirtual("Daytime", window, "DaytimeLine", "1")
 end
+
 
 -- EVENT HANDLER FUNCTIONS ------------------------------------------------------------------------
 
@@ -47,6 +110,9 @@ function Solaris.OnTimelineMoveStop()
     -- Save position after moving control
     Solaris.savedVariables.left = SolarisTimelineControl:GetLeft()
     Solaris.savedVariables.top = SolarisTimelineControl:GetTop()
+end
+
+function Solaris.UpdateRTIPosition()
 end
 
 -- EVENT REGISTRATIONS ----------------------------------------------------------------------------
